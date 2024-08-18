258ce5a252d681d4eb89c5d3a4f76576d9ab6595
Grigori <public@grik.net>
2024-08-19 01:15:55 +0200

    applied feedbacks in PR

--- laravel-example/bootstrap/app-nosession.php
+++ laravel-example/bootstrap/app-nosession.php
@@ -1,24 +1,15 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->web(remove: [StartSession::class,
-            ShareErrorsFromSession::class,
-            ValidateCsrfToken::class,
-        ]);
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+$app = include './app.php';
+$app->withMiddleware(function (Middleware $middleware) {
+    $middleware->web(remove: [StartSession::class,
+        ShareErrorsFromSession::class,
+        ValidateCsrfToken::class,
+    ]);
+});
+return $app;
