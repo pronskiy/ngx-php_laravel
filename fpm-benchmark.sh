6e61dd946db146be05c8e1abc0efa343184a1b82
Grigori <public@grik.net>
2024-08-19 03:35:16 +0200

    Updated URLs in a benchmark shell script

--- fpm-benchmark.sh
+++ fpm-benchmark.sh
@@ -6,15 +6,15 @@ docker build -q -t laravel-fpm-nginx -f ./php-fpm.dockerfile .
 printf "\nLaravel default, no opcache\nRunning ... "
 docker run --name laravel-fpm-default -d --entrypoint /app/fpm-entrypoints/php-fpm-default.sh -p 9803:9803 laravel-fpm-nginx
 sleep 1
-ab -n 500 -c 5 http://localhost:9803/index.php 2>&1 | grep "Requests per second"
+ab -n 500 -c 5 http://localhost:9803/ 2>&1 | grep "Requests per second"
 docker rm -f laravel-fpm-default > /dev/null
 
 printf "\nWith opcache and JIT \nRunning ... "
 docker run --name laravel-fpm-opcache -d --entrypoint /app/fpm-entrypoints/php-fpm-opcache.sh -p 9803:9803 laravel-fpm-nginx
 sleep 1
-ab -n 500 -c 5 http://localhost:9803/index.php 2>&1 | grep "Requests per second"
+ab -n 500 -c 5 http://localhost:9803/ 2>&1 | grep "Requests per second"
 
 printf "\nLaravel without sessions, with opcache \nRunning ... "
-ab -n 500 -c 5 http://localhost:9803/nosession.php 2>&1 | grep "Requests per second"
+ab -n 500 -c 5 http://localhost:9803/nosession 2>&1 | grep "Requests per second"
 docker rm -f laravel-fpm-opcache > /dev/null
 
